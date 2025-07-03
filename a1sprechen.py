97ac262d07b91089243246c9e10eb3cc2bb7a638
learngermanghana <learngermanghana@gmail.com>
2025-07-03 20:57:37 +0000

    Update a1sprechen.py

--- a1sprechen.py
+++ a1sprechen.py
@@ -2177,10 +2177,17 @@ def get_a2_schedule():
             "topic": "Small Talk (Exercise)",
             "chapter": "1.1",
             "goal": "Practice basic greetings and small talk.",
-            "instruction": "Watch the video, review grammar, and complete your workbook.",
+            "instruction": (
+                "Today's lesson has 4 parts:\n\n"
+                 "**1. Sprechen (Group Practice):** Practice the daily question using the brain map provided. Use the chat feature in the Falowen app to speak for at least 1 minute.\n\n"
+                 "**2. Schreiben:** Reframe your group practice as a short letter (assignment).\n\n"
+                 "**3. Lesen:** Complete the reading exercise (7 questions).\n\n"
+                 "**4. Hören:** Do the listening exercise (5 questions).\n\n"
+                 "**5. Assignments to be submitted is schreiben,lesen and horen."
+                 "Finish all sections before submitting your answers."
             "video": "",
-            "grammarbook_link": "",
-            "workbook_link": ""
+            "grammarbook_link": "https://drive.google.com/file/d/1NsCKO4K7MWI-queLWCeBuclmaqPN04YQ/view?usp=sharing",
+            "workbook_link": "https://drive.google.com/file/d/1LXDI1yyJ4aT4LhX5eGDbKnkCkJZ2EE2T/view?usp=sharing"
         },
         # DAY 2
         {
@@ -2190,8 +2197,8 @@ def get_a2_schedule():
             "goal": "Describe people and their appearance.",
             "instruction": "Watch the video, review grammar, and complete your workbook.",
             "video": "",
-            "grammarbook_link": "",
-            "workbook_link": ""
+            "grammarbook_link": "https://drive.google.com/file/d/1VB_nXEfdeTgkzCYjh0tvE75zFJleMlyU/view?usp=sharing",
+            "workbook_link": "https://drive.google.com/file/d/128lWaKgCZ2V-3tActM-dwNy6igLLlzH3/view?usp=sharing"
         },
         # DAY 3
         {
