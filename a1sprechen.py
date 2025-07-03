e1bcad17af50523765dd26853b3590685f880be3
learngermanghana <learngermanghana@gmail.com>
2025-07-03 15:46:20 +0000

    Update a1sprechen.py

--- a1sprechen.py
+++ a1sprechen.py
@@ -1824,7 +1824,7 @@ if tab == "Course":
                 "video": "https://youtu.be/8k2VKKjbekA?si=cgYIJ2Di-OEmOpLJ"
                 "grammarbook_link": "https://drive.google.com/file/d/1D9Pwg29qZ89xh6caAPBcLJ1K671VUc0_/view?usp=sharing",
                 "workbook_link": "https://drive.google.com/file/d/1wjtEyPphP0N7jLbF3AWb5wN_FuJZ5jUQ/view?usp=sharing",
-            },
+            }
         },
         {
             "day": 2,
