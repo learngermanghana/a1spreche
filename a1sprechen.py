fab2e187646c3370c12778bfafc8f21eb147fe7f
learngermanghana <learngermanghana@gmail.com>
2025-07-16 21:23:33 +0000

    Update a1sprechen.py

--- a1sprechen.py
+++ a1sprechen.py
@@ -1014,7 +1014,7 @@ def get_a1_schedule():
         # DAY 20
         {
             "day": 20,
-            "topic": "Introduction to Letter Writing",
+            "topic": "Introduction to Letter Writing 12.3",
             "chapter": "12.3",
             "goal": "Practice how to write both formal and informal letters",
             "assignment": True,
