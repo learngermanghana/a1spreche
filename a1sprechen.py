e68532121bc60b1a8919547d0135c45c7c8d6728
learngermanghana <learngermanghana@gmail.com>
2025-08-23 09:19:31 +0000

    Update a1sprechen.py

--- a1sprechen.py
+++ a1sprechen.py
@@ -14,6 +14,7 @@ import re
 import sqlite3
 import tempfile
 import time
+import urllib.parse as _urllib
 from datetime import date, datetime, timedelta, timezone, UTC
 from typing import Optional
 from uuid import uuid4
