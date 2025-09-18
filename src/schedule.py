75c1983cd3fbabc6442fa1f8482c2b35047380f1
learngermanghana <learngermanghana@gmail.com>
2025-09-18 17:58:15 +0000

    Update schedule.py

--- src/schedule.py
+++ src/schedule.py
@@ -327,7 +327,7 @@ Submitting Work
             "schreiben_sprechen": {
                 "video": "https://youtu.be/XwFPjLjvDog",
                 "youtube_link": "https://youtu.be/XwFPjLjvDog",
-                "workbook_link": "https://drive.google.com/file/d/1wnZehLNfkjgKMFw1V3BX8V399rZg6XLv/view?usp=sharing"
+                "workbook_link": "https://drive.google.com/file/d/1YkajsLxtuQWdhK1P4-bKQHq1BSOev6ac/view?usp=sharing"
             }
         },
         # DAY 15
