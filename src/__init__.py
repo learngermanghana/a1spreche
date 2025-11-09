98a9072277ec6126994a309b00e5f0a5a77e071e
learngermanghana <learngermanghana@gmail.com>
2025-11-09 16:24:18 +0000

    Delete src directory

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Falowen application package.
-
-This file makes the ``src`` directory importable as a package so tests and
-other modules can use standard ``import`` statements.
-"""
-
-# The package currently exposes modules directly rather than defining an API.
--- src/falowen/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Falowen chat utilities package."""
-
-from .chat_core import reset_falowen_chat_flow, back_step, widget_key
-
-__all__ = [
-    "reset_falowen_chat_flow",
-    "back_step",
-    "widget_key",
-]
